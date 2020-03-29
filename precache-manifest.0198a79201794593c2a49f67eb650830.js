a54fc317965733bb89e23965a6743981af9ac6c2
Jaime Caballero <me@jaicab.com>
2020-03-30 00:57:24 +0200

    Updates

---
+++ precache-manifest.0198a79201794593c2a49f67eb650830.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b685a00de16d3d35c60eea52c3a5e84a",
+    "url": "/notion-formulas/index.html"
+  },
+  {
+    "revision": "8049b65dd5d77dbcc4ef",
+    "url": "/notion-formulas/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "951ebb2580a68e27f7ff",
+    "url": "/notion-formulas/static/js/2.2833f18a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/notion-formulas/static/js/2.2833f18a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8049b65dd5d77dbcc4ef",
+    "url": "/notion-formulas/static/js/main.7baf1c6d.chunk.js"
+  },
+  {
+    "revision": "60f5621fcb7b09382e99",
+    "url": "/notion-formulas/static/js/runtime-main.753233b9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/notion-formulas/static/media/logo.5d5d9eef.svg"
+  }
+]);
